8abbdb835de509d56ad1ebc90ead03054312ece7
Antoine Daurat <antoinedaurat@gmail.com>
2022-12-21 16:57:52 +0100

    extend cli api

--- README.md
+++ README.md
@@ -20,27 +20,28 @@ Usage: audioset-dl [OPTIONS]
 Options:
   -o, --output-dir TEXT          target directory for the downloads
                                  (default='./')
-
   -c, --class-name TEXT          the name of the class to download
-                                 (default=Snoring)(list of available classes
-                                 can be printed out with the command
-                                 `audioset-classes`)
-
+                                 (default=Snoring)this option can be repeated
+                                 to select examples at the intersection of
+                                 multiple classese.g. `-c Music -c
+                                 Techno`(list of available classes can be
+                                 printed out with the command `audioset-
+                                 classes`)
   -m, --mixed                    if provided, the downloaded examples will be
                                  instances of `--class-name` and possibly some
                                  other classes. Otherwise (default behaviour),
                                  downloaded examples have only `--class-name`
                                  as single label.
-
   -xe, --exclude-eval-set        if provided, exclude examples from the eval
                                  set (default=False)
-
   -xb, --exclude-balanced-set    if provided, exclude examples from the
                                  balanced set (default=False)
-
   -xu, --exclude-unbalanced-set  if provided, exclude examples from the
                                  unbalanced set (default=False)
-
+  -n, --n-examples INTEGER       number of examples to download (default=all
+                                 matching)
+  -f, --full-source              if provided, download full examples instead
+                                 of 10 sec. segments (default=False)
   --help                         Show this message and exit.
 ```
 
--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ from tqdm import tqdm
 from contextlib import redirect_stderr
 import os
 
-
 __all__ = [
     "audioset_dl",
     "print_classes"
@@ -21,10 +20,12 @@ root = os.path.abspath(os.path.dirname(__file__))
 @click.command()
 @click.option("--output-dir", "-o", type=str, default="./",
               help="target directory for the downloads (default='./')")
-@click.option("--class-name", "-c", type=str, default="Snoring",
+@click.option("--class-name", "-c", type=str, default=["Snoring"], multiple=True,
               help="the name of the class to download (default=Snoring)"
+                   "this option can be repeated to select examples at the intersection of multiple classes"
+                   "e.g. `-c Music -c Techno`"
                    "(list of available classes can be printed out with the command `audioset-classes`)")
-@click.option("--mixed", "-m", default=False, is_flag=True,
+@click.option("--mixed", "-m", is_flag=True,
               help="if provided, the downloaded examples will be instances of `--class-name` "
                    "and possibly some other classes."
                    " Otherwise (default behaviour), downloaded examples have only `--class-name` as single label.")
@@ -34,6 +35,10 @@ root = os.path.abspath(os.path.dirname(__file__))
               help="if provided, exclude examples from the balanced set (default=False)")
 @click.option("--exclude-unbalanced-set", "-xu", is_flag=True,
               help="if provided, exclude examples from the unbalanced set (default=False)")
+@click.option("--n-examples", "-n", type=int, default=None,
+              help="number of examples to download (default=all matching)")
+@click.option("--full-source", "-f", is_flag=True,
+              help="if provided, download full examples instead of 10 sec. segments (default=False)")
 def download_cli(*args, **kwargs):
     """download examples of a specific class from google's AudioSet"""
     audioset_dl(*args, **kwargs)
@@ -48,31 +53,44 @@ def print_classes():
 
 def audioset_dl(
         output_dir="outputs",
-        class_name="Snoring",
+        class_name=("Snoring",),
         mixed=False,
         exclude_eval_set=False,
         exclude_balanced_set=False,
         exclude_unbalanced_set=False,
+        n_examples=None,
+        full_source=False
 ):
     output_dir = os.path.abspath(output_dir)
     ontology = pd.read_json(os.path.join(root, "ontology.json"))
     audioset = pd.DataFrame()
     if not exclude_eval_set:
-        eval_ = pd.read_csv(os.path.join(root, "csv", "eval_segments.csv"), header=2, quotechar='"', skipinitialspace=True)
+        eval_ = pd.read_csv(os.path.join(root, "csv", "eval_segments.csv"), header=2, quotechar='"',
+                            skipinitialspace=True)
         eval_["dir"] = "eval"
         audioset = pd.concat((audioset, eval_))
     if not exclude_balanced_set:
-        balanced = pd.read_csv(os.path.join(root, "csv", "balanced_train_segments.csv"), header=2, quotechar='"', skipinitialspace=True)
+        balanced = pd.read_csv(os.path.join(root, "csv", "balanced_train_segments.csv"), header=2, quotechar='"',
+                               skipinitialspace=True)
         balanced["dir"] = "balanced"
         audioset = pd.concat((audioset, balanced))
 
     if not exclude_unbalanced_set:
-        unbalanced = pd.read_csv(os.path.join(root, "csv", "unbalanced_train_segments.csv"), header=2, quotechar='"', skipinitialspace=True)
+        unbalanced = pd.read_csv(os.path.join(root, "csv", "unbalanced_train_segments.csv"), header=2, quotechar='"',
+                                 skipinitialspace=True)
         unbalanced["dir"] = "unbalanced"
         audioset = pd.concat((audioset, unbalanced))
 
-    cls_id = ontology.id[ontology.name.str.contains(class_name)].item()
-    cls_exmp = audioset.positive_labels.str.contains(cls_id)
+    if mixed or len(class_name) > 1:
+        cls_id = []
+        for name in class_name:
+            cls_id += [ontology.id[ontology.name.str.fullmatch(name)].item()]
+        regex = ''.join(f"(?=.*{w})" for w in cls_id)
+        regex = r'{}'.format(regex)
+        cls_exmp = audioset.positive_labels.str.contains(regex, regex=True)
+    else:
+        cls_id = ontology.id[ontology.name.str.fullmatch(class_name[0])].item()
+        cls_exmp = audioset.positive_labels.str.fullmatch(cls_id)
 
     def _download(exmp):
         if not mixed and exmp.positive_labels != cls_id:
@@ -80,9 +98,10 @@ def audioset_dl(
         start, end = dt.timedelta(seconds=exmp.start_seconds), dt.timedelta(seconds=exmp.end_seconds)
         dler = YoutubeDL({
             "quiet": True, "ignoreerrors": True,
-            "format": "bestaudio", "outtmpl": f"{output_dir}/{class_name}/{exmp.dir}/%(title)s.%(ext)s",
+            "format": "bestaudio", "outtmpl": f"{output_dir}/{exmp.dir}/%(title)s.%(ext)s",
             "external_downloader": "ffmpeg",
-            "external_downloader_args": ["-ss", str(start), "-to", str(end), "-loglevel", "panic"]
+            "external_downloader_args": [*(("-ss", str(start), "-to", str(end)) if not full_source else tuple()),
+                                         "-loglevel", "panic"]
         })
         try:
             with redirect_stderr(io.StringIO()):
@@ -93,6 +112,10 @@ def audioset_dl(
             pass
 
     pool = ThreadPoolExecutor(max_workers=200)
-    futures = [pool.submit(_download, exmp) for exmp in audioset[cls_exmp].itertuples()]
-    for _ in tqdm(as_completed(futures, timeout=None), total=cls_exmp.sum(), file=sys.stdout, ncols=88):
+    subset = audioset[cls_exmp]
+    if n_examples is not None:
+        subset = subset.sample(n=n_examples)
+    futures = [pool.submit(_download, exmp) for exmp in subset.itertuples()]
+    for _ in tqdm(as_completed(futures, timeout=None), total=cls_exmp.sum() if n_examples is None else n_examples,
+                  file=sys.stdout, ncols=88):
         continue
